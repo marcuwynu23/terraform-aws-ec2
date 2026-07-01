590f1e3b5096b457b92ad2b0e1d167088ca90efc
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-02 00:18:21 +0800

    chore: remove AWS provider configuration from main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,1 @@
-# Provider Configuration
-# This block specifies the provider (AWS) and the region where the resources will be created.
-provider "aws" {
-  region = "ap-southeast-1" # Specify the AWS region where resources will be created.
-  # Example regions: us-east-1, eu-west-1, ap-southeast-1 (Singapore), etc.
-}
 
